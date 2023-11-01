9b77dc3402b81b972f1ca14b23704520ff2fe671
stalseq <stalse@bk.ru>
2023-11-01 19:07:04 +0300

    first commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+    private Deth8 deth8;
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( ";
+    }
+}
